ac63fb484c67b9371f963990bd95845beaea3b47
Paul Kilmurray <paul@kilbot.com>
2023-06-13 18:33:11 +0100

    Fix links and add Gist plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,35 +107,6 @@ module.exports = {
       darkTheme: darkCodeTheme,
       additionalLanguages: ['php'],
     },
-    algolia: {
-      // The application ID provided by Algolia
-      appId: 'YOUR_APP_ID',
-
-      // Public API key: it is safe to commit it
-      apiKey: 'YOUR_SEARCH_API_KEY',
-
-      indexName: 'YOUR_INDEX_NAME',
-
-      // Optional: see doc section below
-      contextualSearch: true,
-
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: 'external\\.com|domain\\.com',
-
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      replaceSearchResultPathname: {
-        from: '/docs/', // or as RegExp: /\/docs\//
-        to: '/',
-      },
-
-      // Optional: Algolia search parameters
-      searchParameters: {},
-
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
-
-      //... other Algolia params
-    },
   },
   presets: [
     [
@@ -164,7 +135,7 @@ module.exports = {
   ],
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'fa'],
+    locales: ['en', 'es', 'fr'],
     localeConfigs: {
       en: {
         htmlLang: 'en-GB',
@@ -175,4 +146,5 @@ module.exports = {
       },
     },
   },
+  plugins: ["@orama/plugin-docusaurus"],
 };
