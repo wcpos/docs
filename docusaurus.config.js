7d3713b7f14d7b30395db41594caa1cb073fbfa2
Paul Kilmurray <paul@kilbot.com>
2023-06-22 09:36:40 +0100

    add troubleshooting doc for response error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,7 +164,7 @@ module.exports = {
         },
         gtag: {
           trackingID: 'G-08SJ28P1E5',
-        },
+        }
       },
     ],
   ],
@@ -181,5 +181,16 @@ module.exports = {
       },
     },
   },
-  // plugins: ["@orama/plugin-docusaurus"],
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
 };
